c98dd16cab132280c2f9fdfb6e0a4f3e967ea277
nicatguliyev <nicatguliyev3@gmail.com>
2025-11-15 18:57:26 +0400

    welcome screen yaradildi

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './Screens/WelcomeScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <WelcomeScreen />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Screens/WelcomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const WelcomeScreen = () => {
+
+    return(
+     <View>
+        <Text>SALAM ALEYKUM</Text>
+     </View>
+    );
+
+}
+
+
+export default WelcomeScreen;
